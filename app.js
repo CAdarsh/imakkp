615911da5c245837df0c232d9022cb98eea6b8da
unknown <you@example.com>
2018-12-02 15:49:25 +0530

    first

---
+++ app.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var app = express();
+app.use(express.static('./public'));
+app.get('/',(req,res) => {
+    res.render('app.ejs');
+});
+app.get("/photo_gallery",(req,res) => {
+    res.render('photo.ejs');
+});
+app.get("/video_gallery",(req,res) => {
+    res.render('video.ejs');
+});
+app.listen(3000,console.log('Done'));
