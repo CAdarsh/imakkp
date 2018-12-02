9cb3071de07e398a595402dca007e0a19c4d84c5
unknown <you@example.com>
2018-12-02 16:42:53 +0530

    ''

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var app = express();
+app.set('port',( process.env.PORT || 5000 ));
 app.use(express.static('./public'));
 app.get('/',(req,res) => {
     res.render('app.ejs');
@@ -10,4 +11,4 @@ app.get("/photo_gallery",(req,res) => {
 app.get("/video_gallery",(req,res) => {
     res.render('video.ejs');
 });
-app.listen(3000,console.log('Done'));
+app.listen(app.get('port'),console.log('Done'));
